40b87d097e745e4a95cc9647b832a610934fe955
Federico <fed_v@hotmail.com>
2024-10-05 17:48:29 -0400

    Added Fieldset and Legend to the multi Field component

--- components/forms/FieldArrayComponent.vue
+++ components/forms/FieldArrayComponent.vue
@@ -3,46 +3,56 @@
 
         <div class="form-group" v-for="(entry, idx) in fields" :key="entry.key">
 
-            <div class="input-container">
+            <!-- Grouping phone type and number inputs logically with fieldset -->
+            <fieldset class="phone-fieldset">
 
-                <label 
-                    :id="`phone[${idx}]-label`" 
-                    class="input-label" 
-                    :for="`phone[${idx}].type`">
-                        Phone type
-                </label>
-                
-                <Field 
-                    as="select" 
-                    class="form-input select" 
-                    :name="`phone[${idx}].type`" 
-                    :id="`phone[${idx}].type`" 
-                    :aria-labelledby="`phone[${idx}]-label`" 
-                    rules="required"
+                <!-- Group legend visible only to screen readers -->
+                <legend class="sr-only">
+                    Phone {{ idx + 1 }}
+                </legend>
+
+                <div class="input-container">
+
+                    <label 
+                        :id="`phone[${idx}]-label`" 
+                        class="input-label" 
+                        :for="`phone[${idx}].type`">
+                            Phone type
+                    </label>
+                    
+                    <Field 
+                        as="select" 
+                        class="form-input select" 
+                        :name="`phone[${idx}].type`" 
+                        :id="`phone[${idx}].type`" 
+                        :aria-labelledby="`phone[${idx}]-label`" 
+                        rules="required"
+                    >
+                        <option value="" disabled selected>-- Select --</option>
+                        <option value="home">Home</option>
+                        <option value="mobile">Mobile phone</option>
+                    </Field>
+
+                </div>
+
+                <FormsBaseInput 
+                    type="text" 
+                    label="Phone number" 
+                    :name="`phone[${idx}].number`" 
+                    rules="required" 
+                />
+
+                <button    
+                    class="icon-btn" 
+                    type="button" 
+                    @click="remove(idx)" 
+                    aria-label="Delete phone number {{ idx + 1 }}" 
+                    aria-live="polite"
                 >
-                    <option value="" disabled selected>-- Select --</option>
-                    <option value="home">Home</option>
-                    <option value="mobile">Mobile phone</option>
-                </Field>
+                    <img src="@/assets/images/trash-can.svg" width="16" height="16" title="Delete" alt="Delete">
+                </button>
 
-            </div>
-
-            <FormsBaseInput 
-                type="text" 
-                label="Phone number" 
-                :name="`phone[${idx}].number`" 
-                rules="required" 
-            />
-
-            <button    
-                class="icon-btn" 
-                type="button" 
-                @click="remove(idx)" 
-                aria-label="Delete phone number {{ idx + 1 }}" 
-                aria-live="polite"
-            >
-                <img src="@/assets/images/trash-can.svg" width="16" height="16" title="Delete" alt="Delete">
-            </button>
+            </fieldset>
 
         </div>
 
@@ -65,5 +75,26 @@
     @import '../assets/styles/less/constants.less';
     @import '../assets/styles/less/buttons.less';
     @import '../assets/styles/less/forms.less';
+
+    /* Styles for fieldset and legend */
+    .phone-fieldset {
+        display: flex;
+        border: none;
+        padding: 0;
+        margin-bottom: 10px;
+        width: 100%;
+    }
+
+    /* Hide the legend visually but keep it accessible to screen readers */
+    .sr-only {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        margin: -1px;
+        padding: 0;
+        overflow: hidden;
+        clip: rect(0, 0, 0, 0);
+        border: 0;
+    }
     
 </style>
