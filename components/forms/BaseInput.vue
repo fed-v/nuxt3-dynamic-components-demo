46d6c6affbdd4815c6b4a87aa4f7dd09a3bfce62
Federico <fed_v@hotmail.com>
2024-10-05 03:53:51 -0400

    Added accessiblity improvements

--- components/forms/BaseInput.vue
+++ components/forms/BaseInput.vue
@@ -5,15 +5,38 @@
         <label class="input-label" :for="name">{{ label }}</label>
 
         <!-- The default value of this Input can be passed via v-model. When the value is changed, it will emit its value to the parent -->
-        <Field class="form-input" :rules="rules" :class="error" :type="type" :id="name" :name="name" :value="boundValue" @input="$emit('update:boundValue', $event.target.value)" />
-        <ErrorMessage class="error-msg" :name="name" />
+        <Field
+            :id="name"
+            class="form-input"
+            :rules="rules"
+            :class="`${classes ? classes : ''} ${error ? error : ''}`"
+            :type="type"
+            :name="name"
+            :value="boundValue"
+            :disabled="disabled"
+            :aria-invalid="error[name] ? 'true' : 'false'"
+            :aria-describedby="`${name}-error`"
+            :autocomplete="autocomplete"
+            @input="$emit('update:boundValue', $event.target.value)"
+        />
+        <ErrorMessage class="error-msg" :name="name" role="alert" />
         
     </div>
 </template>
 
 <script lang="ts" setup>
 
-    const props = defineProps({
+const {
+    type,
+    name,
+    label,
+    rules,
+    error,
+    boundValue,
+    disabled,
+    classes,
+    autocomplete,
+  } = defineProps({
         type: {
             type: String,
             required: true,
@@ -38,6 +61,20 @@
             type: String,
             required: false,
         },
+        disabled: {
+            type: Boolean,
+            required: false,
+        },
+        classes: {
+            type: String,
+            required: false,
+            default: '',
+        },
+        autocomplete: {
+            type: String,
+            required: false,
+            default: 'off', // Default to 'off' if not specified
+        }
         
     });
 
